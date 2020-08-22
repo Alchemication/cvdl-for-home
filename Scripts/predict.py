e38d7b37075f46c32af2e01780d3c2b52e928c80
Alchemication <you@example.com>
2020-08-22 09:47:37 +0100

    Working on train/predict Python scripts

--- Scripts/predict.py
+++ Scripts/predict.py
@@ -1,10 +1,16 @@
 # predict probabilities for counts, given a set of future features
 # execute: python predict.py
+# This file can be scheduled to run at any time.
+# It will generate hourly predictions for the remaining hours in today and N-additional days:
+# - current hour - if the job is running at 8:45, the first prediction will be for time between 8AM and 9AM
+# - next hours - till the end of the day, so until 23:59
+# - and next N-days (so for 2 extra days - 48 additional predictions)
 
 # Import necessary libraries
 import logging
 import pickle
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
 from tqdm import tqdm
 import requests
@@ -14,7 +20,11 @@ import os
 # Define constants to use
 DATASETS_PATH = '../Datasets'
 OBJECT_CLASSES = ['person', 'vehicle']
+PICKLED_SCALER_FILE = 'final_scaler__%%OBJECT_NAME%%.pickle'
 PICKLED_MODEL_FILE = 'final_model__%%OBJECT_NAME%%.pickle'
+PREDICTIONS_FILE = 'final_predictions__%%OBJECT_NAME%%.csv'
+N_SAMPLES = 2500  # how many simulations to execute in a Poisson process
+PROBA_THRESH = 0.05  # reject probabilities below this threshold
 
 # Define weather API parameters
 API_BASE_URL = 'https://api.darksky.net/forecast'
@@ -42,16 +52,25 @@ def predict(n_days: int = 3) -> None:
     Create predictions for the next N days (including today)
     (meaning hours remaining for today + extra 2 days)
     :param n_days: int
-    :return:
+    :return: None
     """
 
-    # fetch weather data
-
-    # start date will be 45 minutes after current hour
+    # start date will be 30 minutes after current hour
     start_date = datetime.now().replace(minute=30, second=0)
 
     # end date will be + N-days at 23:45
     end_date = (start_date + timedelta(days=n_days-1)).replace(hour=23)
+
+    # generate hourly date ranges
+    idx = pd.date_range(start=start_date, end=end_date, freq='1H')
+    hourly_df = pd.DataFrame({'dt': idx})
+
+    # add date-time related features
+    hourly_df['date'] = hourly_df['dt'].astype(str).str[0:19]
+    hourly_df['hour'] = hourly_df['dt'].dt.hour
+    hourly_df['n_month'] = hourly_df['dt'].dt.month
+    hourly_df['day_of_week'] = hourly_df['dt'].dt.dayofweek
+    hourly_df['is_weekend_day'] = (hourly_df['dt'].dt.dayofweek // 5 == 1).astype(int)
 
     # check if cached file with weather already exists, and use it
     # (to avoid making expensive API calls)
@@ -62,19 +81,15 @@ def predict(n_days: int = 3) -> None:
     else:
         logging.info(f'Pulling weather data from API')
 
-        # generate hourly date ranges
-        idx = pd.date_range(start=start_date, end=end_date, freq='1H')
-        df = pd.DataFrame({'dt': idx})
-
         # pull weather data for each hour from API
         dark_sky = []
         prev_cur_obj = {}
         prev_daily_obj = {}
 
-        for index, d in tqdm(df.iterrows(), total=df.shape[0]):
+        for index, d in tqdm(hourly_df.iterrows(), total=hourly_df.shape[0]):
 
             # extract date-time info from dt object
-            args = (d.dt.year, d.dt.month, d.dt.day, d.dt.hour, d.dt.minute)
+            args = (d['dt'].year, d['dt'].month, d['dt'].day, d['dt'].hour, d['dt'].minute)
             ts = make_ts(*args)
 
             # make a call to Dark Sky API
@@ -89,7 +104,7 @@ def predict(n_days: int = 3) -> None:
             daily = sky_data['daily']
 
             # init object to save
-            cur_obj = {'ts': ts, 'dt': d}
+            cur_obj = {'dt': d['dt']}
 
             # keep only selected data elements
             for k in cur_keys:
@@ -108,19 +123,81 @@ def predict(n_days: int = 3) -> None:
 
         logging.info(f'Caching weather data')
         weather_df = pd.DataFrame(dark_sky)
-        weather_df['dt'] = df['dt']
         weather_df.to_csv(cached_filename, index=False)
 
-    # load models
+    # add date-time related features, so we can merge the datasets together
+    weather_df['dt'] = pd.to_datetime(weather_df['dt'])
+    weather_df['date'] = weather_df['dt'].astype(str).str[0:19]
+
+    # join detections and weather data
+    logging.info(f'Merging hourly and weather datasets')
+    merged = hourly_df.merge(weather_df, on=['date'])
+    assert hourly_df.shape[0] == merged.shape[0]
 
     # generate dataset for prediction
+    logging.info(f'Preparing features for predictions')
+    use_cols = ['hour', 'n_month', 'day_of_week', 'is_weekend_day', 'cur__precipIntensity',
+                'cur__precipProbability', 'cur__apparentTemperature', 'cur__humidity', 'cur__windSpeed',
+                'cur__uvIndex']
+    X = merged[use_cols]
 
-    # predict
+    # load models for object classes and generate predictions
+    for ob_class in OBJECT_CLASSES:
+        logging.info(f'Loading scaler for {ob_class}')
+        with open(f'{DATASETS_PATH}/{PICKLED_SCALER_FILE.replace("%%OBJECT_NAME%%", ob_class)}', 'rb') as f:
+            scaler = pickle.load(f)
 
-    # generate probabilities from predictions
+        # scale features using pickled scaler
+        logging.info(f'Scaling features for {ob_class}')
+        X_scaled = scaler.transform(X)
 
-    # persist latest predictions and probabilities
-    pass
+        logging.info(f'Loading model for {ob_class}')
+        with open(f'{DATASETS_PATH}/{PICKLED_MODEL_FILE.replace("%%OBJECT_NAME%%", ob_class)}', 'rb') as f:
+            model = pickle.load(f)
+
+        logging.info(f'Generating predictions for {ob_class}')
+        predictions = model.predict(X_scaled)
+
+        logging.info(f'Generating probabilities from predictions for {ob_class}')
+        predictions_probas = []
+        expected_counts = []
+
+        # sample from Poisson probability distribution for each prediction,
+        # the assumption is that predictions can be interpreted as the rates
+        # in a Poisson process (https://en.wikipedia.org/wiki/Poisson_distribution)
+        for event_rate in predictions:
+            samples = np.random.poisson(lam=event_rate, size=N_SAMPLES)
+
+            # count unique counts from the simulation
+            numbers, counts = np.unique(samples, return_counts=True)
+
+            # generate probabilities from the unique counts
+            probabilities = counts / counts.sum()
+
+            # index with highest value will represent the count to expect
+            expected_count = np.argmax(probabilities)
+            expected_counts.append(expected_count)
+
+            # filter out weak probabilities
+            numbers_filtered = numbers[probabilities > PROBA_THRESH]
+            counts_filtered = counts[probabilities > PROBA_THRESH]
+            probabilities_filtered = probabilities[probabilities > PROBA_THRESH]
+            predictions_probas.append({
+                'numbers': numbers_filtered,
+                'counts': counts_filtered,
+                'probas': probabilities_filtered,
+            })
+
+        # create a copy of X with added predictions
+        X_cp = X.copy()
+        X_cp['pred'] = predictions
+        X_cp['expected_count'] = expected_counts
+        X_cp['pred_proba'] = predictions_probas
+
+        # export to csv (for now), ideally this should be persisted
+        # in the DB, so the results can be analyzed later on
+        logging.info(f'Saving probabilities for {ob_class}')
+        X_cp.to_csv(f'{DATASETS_PATH}/{PREDICTIONS_FILE.replace("%%OBJECT_NAME%%", ob_class)}', index=False)
 
 
 if __name__ == '__main__':
--- Scripts/train.py
+++ Scripts/train.py
@@ -1,9 +1,6 @@
 # train models for selected object classes
 # execute: python train.py
 
-# this is a very first release of this script,
-# so plenty of hard-coding in place!
-
 # Import necessary libraries
 import pandas as pd
 import numpy as np
@@ -29,7 +26,9 @@ DETECTIONS_FILENAME = 'UniqueObjectDetections__%%OBJECT_NAME%%__2019-09-09_2020-
 WEATHER_FILENAME = 'dark_sky_data_2019-09-09_2020-03-02.csv'
 OBJECT_CLASSES = ['person', 'vehicle']
 DOWNTIME_DATES = ['2020-01-13', '2020-01-14', '2020-02-28']
+PICKLED_SCALER_FILE = 'final_scaler__%%OBJECT_NAME%%.pickle'
 PICKLED_MODEL_FILE = 'final_model__%%OBJECT_NAME%%.pickle'
+RUN_CROSS_VAL = False
 
 
 def train() -> None:
@@ -87,9 +86,6 @@ def train() -> None:
         # join detections and weather data
         merged = object_detections.merge(weather, on=['date', 'hour'])
 
-        # as a sanity check, run a cross validation and display results
-        logging.info('Running cross validation')
-
         # define features to use
         use_cols = ['hour', 'n_month', 'day_of_week', 'is_weekend_day', 'cur__precipIntensity',
                     'cur__precipProbability', 'cur__apparentTemperature', 'cur__humidity', 'cur__windSpeed',
@@ -99,49 +95,55 @@ def train() -> None:
         X = merged[use_cols]
         y = merged['obs_count']
 
-        # KFold Cross Validation approach
-        kf = KFold(n_splits=5, shuffle=True)
-        # Iterate over each train-test split and calculate scores
-        mpds = []
-        mses = []
-        maes = []
-        r2s = []
-        acc = []
-        for train_index, test_index in tqdm(kf.split(X)):
-            # Split train-test
-            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-            # Scale train and test sets
-            scaler = StandardScaler()
-            X_train_scaled = scaler.fit_transform(X_train)
-            X_test_scaled = scaler.transform(X_test)
-            # Train model
-            model = HistGradientBoostingRegressor(loss='poisson', learning_rate=0.01, max_iter=550,
-                                                  max_bins=52, l2_regularization=0.1, verbose=0,
-                                                  validation_fraction=0.1, n_iter_no_change=5,
-                                                  early_stopping=True)
-            model.fit(X_train_scaled, y_train)
-            # Make predictions and round them off
-            y_pred = model.predict(X_test_scaled)
-            y_pred_rounded = np.array([round(p) for p in y_pred])
-            # Calculate scores
-            mpds.append(mean_poisson_deviance(y_test, y_pred + 0.00001))
-            mses.append(mean_squared_error(y_test, y_pred))
-            maes.append(mean_absolute_error(y_test, y_pred))
-            r2s.append(r2_score(y_test, y_pred))
-            acc.append(accuracy_score(y_test, y_pred_rounded))
+        # check if KFold Cross Validation is required
+        if RUN_CROSS_VAL:
 
-        # log stats (ideally this should be persisted)
-        logging.info(f'mean mpd for {ob_class}:, {np.mean(mpds)}')
-        logging.info(f'mean mse for {ob_class}:, {np.mean(mses)}')
-        logging.info(f'mean mae for {ob_class}:, {np.mean(maes)}')
-        logging.info(f'mean r2 for {ob_class}:, {np.mean(r2s)}')
-        logging.info(f'mean acc for {ob_class}:, {np.mean(acc)}')
+            # run a cross validation and display results
+            logging.info('Running cross validation')
 
-        # now create a model without train/test split
+            kf = KFold(n_splits=5, shuffle=True)
+            # Iterate over each train-test split and calculate scores
+            mpds = []
+            mses = []
+            maes = []
+            r2s = []
+            acc = []
+            for train_index, test_index in tqdm(kf.split(X)):
+                # Split train-test
+                X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+                y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+                # Scale train and test sets
+                scaler = StandardScaler()
+                X_train_scaled = scaler.fit_transform(X_train)
+                X_test_scaled = scaler.transform(X_test)
+                # Train model
+                model = HistGradientBoostingRegressor(loss='poisson', learning_rate=0.01, max_iter=550,
+                                                      max_bins=52, l2_regularization=0.1, verbose=0,
+                                                      validation_fraction=0.1, n_iter_no_change=5,
+                                                      early_stopping=True)
+                model.fit(X_train_scaled, y_train)
+                # Make predictions and round them off
+                y_pred = model.predict(X_test_scaled)
+                y_pred_rounded = np.array([round(p) for p in y_pred])
+                # Calculate scores
+                mpds.append(mean_poisson_deviance(y_test, y_pred + 0.00001))
+                mses.append(mean_squared_error(y_test, y_pred))
+                maes.append(mean_absolute_error(y_test, y_pred))
+                r2s.append(r2_score(y_test, y_pred))
+                acc.append(accuracy_score(y_test, y_pred_rounded))
+
+            # log stats (ideally this should be persisted)
+            logging.info(f'mean mpd for {ob_class}:, {np.mean(mpds)}')
+            logging.info(f'mean mse for {ob_class}:, {np.mean(mses)}')
+            logging.info(f'mean mae for {ob_class}:, {np.mean(maes)}')
+            logging.info(f'mean r2 for {ob_class}:, {np.mean(r2s)}')
+            logging.info(f'mean acc for {ob_class}:, {np.mean(acc)}')
+
+        # now scale whole dataset (without train/test split)
         scaler = StandardScaler()
         X_scaled = scaler.fit_transform(X)
-        # Train model
+
+        # train model using scaled, whole dataset
         logging.info('Training model on all data')
         model = HistGradientBoostingRegressor(loss='poisson', learning_rate=0.01, max_iter=550,
                                               max_bins=52, l2_regularization=0.1, verbose=0,
@@ -149,7 +151,14 @@ def train() -> None:
                                               early_stopping=True)
         model.fit(X_scaled, y)
 
+        # save scaler as pickle
+        logging.info(f'Saving scaler for {ob_class}')
+        scaler_filename = f'{DATASETS_PATH}/{PICKLED_SCALER_FILE.replace("%%OBJECT_NAME%%", ob_class)}'
+        with open(scaler_filename, 'wb') as f:
+            pickle.dump(scaler, f)
+
         # save model as pickle
+        logging.info(f'Saving model for {ob_class}')
         model_filename = f'{DATASETS_PATH}/{PICKLED_MODEL_FILE.replace("%%OBJECT_NAME%%", ob_class)}'
         with open(model_filename, 'wb') as f:
             pickle.dump(model, f)
